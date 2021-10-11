82305c01a7ecd70a3a2ed29a501102b1cca9e460
chio-san <32412086+chio-san@users.noreply.github.com>
2021-10-11 20:04:46 +0600

    init

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    f = open('googs.txt', 'r+', encoding='utf-8')
+    f = open('goods.txt', 'r+', encoding='utf-8')
     txt = f.readlines()
     return render_template('index.html', goods=txt)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('goods.txt', 'a+', encoding='utf-8')
+    f.write(good + "\n")
+    f.close()
+    return "Инвентарь пополнен"
